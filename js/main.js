62a58794eaff054b50d56d15506e1ee8d9045760
marcel tumboimbela <jonathan.marcel.t@gmail.com>
2019-12-01 15:24:57 +0700

    add dota 2 heroes list

---
+++ js/main.js
@@ -0,0 +1,484 @@
+let heroes = [
+    {
+      "name": "Abaddon",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/2/26/Abaddon_icon.png"
+    },
+    {
+      "name": "Alchemist",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/f/fe/Alchemist_icon.png"
+    },
+    {
+      "name": "Axe",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/2/23/Axe_icon.png"
+    },
+    {
+      "name": "Beastmaster",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/d/d9/Beastmaster_icon.png"
+    },
+    {
+      "name": "Brewmaster",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/1/1e/Brewmaster_icon.png"
+    },
+    {
+      "name": "Bristleback",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/4/4d/Bristleback_icon.png"
+    },
+    {
+      "name": "Centaur Warrunner",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/e/ed/Centaur_Warrunner_icon.png"
+    },
+    {
+      "name": "Chaos Knight",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/f/fe/Chaos_Knight_icon.png"
+    },
+    {
+      "name": "Clockwerk",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/d/d8/Clockwerk_icon.png"
+    },
+    {
+      "name": "Doom",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/4/40/Doom_icon.png"
+    },
+    {
+      "name": "Dragon Knight",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/5/59/Dragon_Knight_icon.png"
+    },
+    {
+      "name": "Earth Spirit",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/b/be/Earth_Spirit_icon.png"
+    },
+    {
+      "name": "Earthshaker",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/a/a5/Earthshaker_icon.png"
+    },
+    {
+      "name": "Elder Titan",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/1/1a/Elder_Titan_icon.png"
+    },
+    {
+      "name": "Huskar",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/d/d3/Huskar_icon.png"
+    },
+    {
+      "name": "Io",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/8/8d/Io_icon.png"
+    },
+    {
+      "name": "Kunkka",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/c/c0/Kunkka_icon.png"
+    },
+    {
+      "name": "Legion Commander",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/a/a2/Legion_Commander_icon.png"
+    },
+    {
+      "name": "Lifestealer",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/2/2b/Lifestealer_icon.png"
+    },
+    {
+      "name": "Lycan",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/d/d6/Lycan_icon.png"
+    },
+    {
+      "name": "Magnus",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/b/ba/Magnus_icon.png"
+    },
+    {
+      "name": "Mars",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/9/9d/Mars_icon.png"
+    },
+    {
+      "name": "Night Stalker",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/1/15/Night_Stalker_icon.png"
+    },
+    {
+      "name": "Omniknight",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/e/e2/Omniknight_icon.png"
+    },
+    {
+      "name": "Phoenix",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/1/14/Phoenix_icon.png"
+    },
+    {
+      "name": "Pudge",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/c/c0/Pudge_icon.png"
+    },
+    {
+      "name": "Sand King",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/7/79/Sand_King_icon.png"
+    },
+    {
+      "name": "Slardar",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/7/7e/Slardar_icon.png"
+    },
+    {
+      "name": "Snapfire",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/7/7a/Snapfire_icon.png"
+    },
+    {
+      "name": "Spirit Breaker",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/d/df/Spirit_Breaker_icon.png"
+    },
+    {
+      "name": "Sven",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/1/1b/Sven_icon.png"
+    },
+    {
+      "name": "Tidehunter",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/d/d5/Tidehunter_icon.png"
+    },
+    {
+      "name": "Timbersaw",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/9/9a/Timbersaw_icon.png"
+    },
+    {
+      "name": "Tiny",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/5/55/Tiny_icon.png"
+    },
+    {
+      "name": "Treant Protector",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/3/3f/Treant_Protector_icon.png"
+    },
+    {
+      "name": "Tusk",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/c/ce/Tusk_icon.png"
+    },
+    {
+      "name": "Underlord",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/1/18/Underlord_icon.png"
+    },
+    {
+      "name": "Undying",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/6/61/Undying_icon.png"
+    },
+    {
+      "name": "Wraith King",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/1/1e/Wraith_King_icon.png"
+    },
+    {
+      "name": "Anti-Mage",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/8/8e/Anti-Mage_icon.png"
+    },
+    {
+      "name": "Arc Warden",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/0/07/Arc_Warden_icon.png"
+    },
+    {
+      "name": "Bloodseeker",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/5/56/Bloodseeker_icon.png"
+    },
+    {
+      "name": "Bounty Hunter",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/a/a6/Bounty_Hunter_icon.png"
+    },
+    {
+      "name": "Broodmother",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/d/df/Broodmother_icon.png"
+    },
+    {
+      "name": "Clinkz",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/c/cb/Clinkz_icon.png"
+    },
+    {
+      "name": "Drow Ranger",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/8/80/Drow_Ranger_icon.png"
+    },
+    {
+      "name": "Ember Spirit",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/9/91/Ember_Spirit_icon.png"
+    },
+    {
+      "name": "Faceless Void",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/7/73/Faceless_Void_icon.png"
+    },
+    {
+      "name": "Gyrocopter",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/4/4f/Gyrocopter_icon.png"
+    },
+    {
+      "name": "Juggernaut",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/0/03/Juggernaut_icon.png"
+    },
+    {
+      "name": "Lone Druid",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/5/5d/Lone_Druid_icon.png"
+    },
+    {
+      "name": "Luna",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/7/7d/Luna_icon.png"
+    },
+    {
+      "name": "Medusa",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/c/cc/Medusa_icon.png"
+    },
+    {
+      "name": "Meepo",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/8/85/Meepo_icon.png"
+    },
+    {
+      "name": "Mirana",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/1/12/Mirana_icon.png"
+    },
+    {
+      "name": "Monkey King",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/7/7b/Monkey_King_icon.png"
+    },
+    {
+      "name": "Morphling",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/7/7b/Morphling_icon.png"
+    },
+    {
+      "name": "Naga Siren",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/6/60/Naga_Siren_icon.png"
+    },
+    {
+      "name": "Nyx Assassin",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/f/fa/Nyx_Assassin_icon.png"
+    },
+    {
+      "name": "Pangolier",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/4/4e/Pangolier_icon.png"
+    },
+    {
+      "name": "Phantom Assassin",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/8/8e/Phantom_Assassin_icon.png"
+    },
+    {
+      "name": "Phantom Lancer",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/8/81/Phantom_Lancer_icon.png"
+    },
+    {
+      "name": "Razor",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/6/66/Razor_icon.png"
+    },
+    {
+      "name": "Riki",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/7/7d/Riki_icon.png"
+    },
+    {
+      "name": "Shadow Fiend",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/3/36/Shadow_Fiend_icon.png"
+    },
+    {
+      "name": "Slark",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/a/aa/Slark_icon.png"
+    },
+    {
+      "name": "Sniper",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/5/51/Sniper_icon.png"
+    },
+    {
+      "name": "Spectre",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/f/ff/Spectre_icon.png"
+    },
+    {
+      "name": "Templar Assassin",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/9/9c/Templar_Assassin_icon.png"
+    },
+    {
+      "name": "Terrorblade",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/5/52/Terrorblade_icon.png"
+    },
+    {
+      "name": "Troll Warlord",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/f/f0/Troll_Warlord_icon.png"
+    },
+    {
+      "name": "Ursa",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/4/40/Ursa_icon.png"
+    },
+    {
+      "name": "Vengeful Spirit",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/2/20/Vengeful_Spirit_icon.png"
+    },
+    {
+      "name": "Venomancer",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/2/25/Venomancer_icon.png"
+    },
+    {
+      "name": "Viper",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/5/5f/Viper_icon.png"
+    },
+    {
+      "name": "Weaver",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/0/09/Weaver_icon.png"
+    },
+    {
+      "name": "Ancient Apparition",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/6/67/Ancient_Apparition_icon.png"
+    },
+    {
+      "name": "Bane",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/c/c3/Bane_icon.png"
+    },
+    {
+      "name": "Batrider",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/f/f2/Batrider_icon.png"
+    },
+    {
+      "name": "Chen",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/6/61/Chen_icon.png"
+    },
+    {
+      "name": "Crystal Maiden",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/2/27/Crystal_Maiden_icon.png"
+    },
+    {
+      "name": "Dark Seer",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/c/c5/Dark_Seer_icon.png"
+    },
+    {
+      "name": "Dark Willow",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/3/3c/Dark_Willow_icon.png"
+    },
+    {
+      "name": "Dazzle",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/e/e6/Dazzle_icon.png"
+    },
+    {
+      "name": "Death Prophet",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/d/d7/Death_Prophet_icon.png"
+    },
+    {
+      "name": "Disruptor",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/9/97/Disruptor_icon.png"
+    },
+    {
+      "name": "Enchantress",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/4/41/Enchantress_icon.png"
+    },
+    {
+      "name": "Enigma",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/f/f7/Enigma_icon.png"
+    },
+    {
+      "name": "Grimstroke",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/d/d7/Grimstroke_icon.png"
+    },
+    {
+      "name": "Invoker",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/0/00/Invoker_icon.png"
+    },
+    {
+      "name": "Jakiro",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/2/2f/Jakiro_icon.png"
+    },
+    {
+      "name": "Keeper of the Light",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/b/b9/Keeper_of_the_Light_icon.png"
+    },
+    {
+      "name": "Leshrac",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/2/26/Leshrac_icon.png"
+    },
+    {
+      "name": "Lich",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/b/bb/Lich_icon.png"
+    },
+    {
+      "name": "Lina",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/3/35/Lina_icon.png"
+    },
+    {
+      "name": "Lion",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/b/b8/Lion_icon.png"
+    },
+    {
+      "name": "Nature's Prophet",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/c/c4/Nature%27s_Prophet_icon.png"
+    },
+    {
+      "name": "Necrophos",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/a/a6/Necrophos_icon.png"
+    },
+    {
+      "name": "Ogre Magi",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/e/e0/Ogre_Magi_icon.png"
+    },
+    {
+      "name": "Oracle",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/7/72/Oracle_icon.png"
+    },
+    {
+      "name": "Outworld Devourer",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/9/99/Outworld_Devourer_icon.png"
+    },
+    {
+      "name": "Puck",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/1/13/Puck_icon.png"
+    },
+    {
+      "name": "Pugna",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/c/cd/Pugna_icon.png"
+    },
+    {
+      "name": "Queen of Pain",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/a/a1/Queen_of_Pain_icon.png"
+    },
+    {
+      "name": "Rubick",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/8/8a/Rubick_icon.png"
+    },
+    {
+      "name": "Shadow Demon",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/f/f3/Shadow_Demon_icon.png"
+    },
+    {
+      "name": "Shadow Shaman",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/9/96/Shadow_Shaman_icon.png"
+    },
+    {
+      "name": "Silencer",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/9/9f/Silencer_icon.png"
+    },
+    {
+      "name": "Skywrath Mage",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/b/bf/Skywrath_Mage_icon.png"
+    },
+    {
+      "name": "Storm Spirit",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/1/13/Storm_Spirit_icon.png"
+    },
+    {
+      "name": "Techies",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/f/fa/Techies_icon.png"
+    },
+    {
+      "name": "Tinker",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/d/d1/Tinker_icon.png"
+    },
+    {
+      "name": "Visage",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/9/9e/Visage_icon.png"
+    },
+    {
+      "name": "Void Spirit",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/9/99/Void_Spirit_icon.png"
+    },
+    {
+      "name": "Warlock",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/3/3f/Warlock_icon.png"
+    },
+    {
+      "name": "Windranger",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/6/60/Windranger_icon.png"
+    },
+    {
+      "name": "Winter Wyvern",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/4/4a/Winter_Wyvern_icon.png"
+    },
+    {
+      "name": "Witch Doctor",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/3/33/Witch_Doctor_icon.png"
+    },
+    {
+      "name": "Zeus",
+      "icon": "https://gamepedia.cursecdn.com/dota2_gamepedia/3/3f/Zeus_icon.png"
+    }
+]
+
+let container = document.querySelector(".container");
+heroes.forEach(hero => {
+    let heroHTML = `<div class="hero-portrait__container"><div class="hero-portrait__image" style="background-image:url(${hero.icon})"></div><span class="hero-portrait__name">${hero.name}</span></div>`
+    container.innerHTML += heroHTML
+})
